6ed722d93618bbbba51d74656167a68b46a40899
Vladislav Lobanov <vladislovelobanov@gmail.com>
2025-07-03 04:14:19 +0400

    Nvim

---
+++ dot_config/nvim/lua/kickstart/plugins/debug.lua
@@ -0,0 +1,234 @@
+-- debug.lua
+--
+-- Shows how to use the DAP plugin to debug your code.
+--
+-- Primarily focused on configuring the debugger for Go, but can
+-- be extended to other languages as well. That's why it's called
+-- kickstart.nvim and not kitchen-sink.nvim ;)
+
+return {
+  'mfussenegger/nvim-dap',
+  dependencies = {
+    'rcarriga/nvim-dap-ui',
+    'nvim-neotest/nvim-nio',
+    'theHamsta/nvim-dap-virtual-text',
+    'mason-org/mason.nvim',
+    'jay-babu/mason-nvim-dap.nvim',
+    'leoluz/nvim-dap-go',
+  },
+  keys = {
+    {
+      '<F1>',
+      function()
+        require('dap').step_into()
+      end,
+      desc = 'Debug: Step Into',
+    },
+    {
+      '<F2>',
+      function()
+        require('dap').step_over()
+      end,
+      desc = 'Debug: Step Over',
+    },
+    {
+      '<F3>',
+      function()
+        require('dap').step_back()
+      end,
+      desc = 'Debug: Step Back',
+    },
+    {
+      '<F4>',
+      function()
+        require('dap').step_out()
+      end,
+      desc = 'Debug: Step Out',
+    },
+    {
+      '<F5>',
+      function()
+        require('dap').continue()
+      end,
+      desc = 'Debug: Start/Continue',
+    },
+    {
+      '<F6>',
+      function()
+        require('dap').run_to_cursor()
+      end,
+      desc = 'Debug: Start/Continue',
+    },
+    {
+      '<F7>',
+      function()
+        require('dapui').toggle()
+      end,
+      desc = 'Debug: See last session result.',
+    },
+    {
+      '<leader>b',
+      function()
+        require('dap').toggle_breakpoint()
+      end,
+      desc = 'Debug: Toggle Breakpoint',
+    },
+    {
+      '<leader>B',
+      function()
+        require('dap').set_breakpoint(vim.fn.input 'Breakpoint condition: ')
+      end,
+      desc = 'Debug: Set Breakpoint',
+    },
+    {
+      '<leader>?',
+      function()
+        require('dapui').eval(nil, {
+          enter = true,
+          context = 'hover',
+          width = 80,
+          height = 10,
+        })
+      end,
+      desc = 'DAP UI: Evaluate expression at cursor',
+    },
+    {
+      '<F12>',
+      function()
+        local dap = require 'dap'
+        dap.terminate()
+        vim.defer_fn(function()
+          dap.run_last()
+        end, 100)
+      end,
+      desc = 'Debug: Restart session',
+    },
+  },
+  config = function()
+    local dap = require 'dap'
+    local dapui = require 'dapui'
+
+    -- Store original layouts
+    local original_layouts = {
+      {
+        elements = {
+          { id = 'scopes', size = 0.25 },
+          { id = 'breakpoints', size = 0.25 },
+          { id = 'stacks', size = 0.25 },
+          { id = 'watches', size = 0.25 },
+        },
+        size = 40,
+        position = 'left',
+      },
+      {
+        elements = {
+          { id = 'repl', size = 0.5 },
+          { id = 'console', size = 0.5 },
+        },
+        size = 10,
+        position = 'bottom',
+      },
+    }
+
+    require('mason-nvim-dap').setup {
+      automatic_installation = true,
+      handlers = {},
+      ensure_installed = {
+        'delve',
+        'codelldb',
+        'debugpy',
+      },
+    }
+    require('nvim-dap-virtual-text').setup {
+      display_callback = function(variable)
+        if #variable.value > 15 then
+          return ' ' .. string.sub(variable.value, 1, 15) .. '... '
+        end
+        return ' ' .. variable.value
+      end,
+    }
+
+    dapui.setup {
+      mappings = {
+        expand = '<CR>',
+        open = 'o',
+        remove = 'd',
+        edit = 'e',
+        repl = 'r',
+        toggle = 't',
+      },
+      element_mappings = {},
+      expand_lines = true,
+      force_buffers = true,
+      layouts = original_layouts,
+      floating = {
+        border = 'rounded',
+        mappings = {
+          close = { 'q', '<Esc>' },
+        },
+      },
+      render = {
+        indent = 4,
+      },
+      icons = { expanded = '▾', collapsed = '▸', current_frame = '*' },
+      controls = {
+        icons = {
+          pause = '⏸',
+          play = '▶',
+          step_into = '⏎',
+          step_over = '⏭',
+          step_out = '⏮',
+          step_back = 'b',
+          run_last = '▶▶',
+          terminate = '⏹',
+          disconnect = '⏏',
+        },
+        enabled = true,
+        element = 'repl',
+      },
+    }
+
+    vim.api.nvim_set_hl(0, 'DapBreak', { fg = '#e51400' })
+    vim.api.nvim_set_hl(0, 'DapStop', { fg = '#ffcc00' })
+    local breakpoint_icons = vim.g.have_nerd_font
+        and { Breakpoint = '', BreakpointCondition = '', BreakpointRejected = '', LogPoint = '', Stopped = '' }
+      or { Breakpoint = '●', BreakpointCondition = '⊜', BreakpointRejected = '⊘', LogPoint = '◆', Stopped = '⭔' }
+    for type, icon in pairs(breakpoint_icons) do
+      local tp = 'Dap' .. type
+      local hl = (type == 'Stopped') and 'DapStop' or 'DapBreak'
+      vim.fn.sign_define(tp, { text = icon, texthl = hl, numhl = hl })
+    end
+
+    dap.listeners.after.event_initialized['dapui_config'] = dapui.open
+    dap.listeners.before.event_terminated['dapui_config'] = dapui.close
+    dap.listeners.before.event_exited['dapui_config'] = dapui.close
+
+    require('dap-go').setup {
+      delve = {
+        detached = vim.fn.has 'win32' == 0,
+      },
+    }
+
+    dap.adapters.codelldb = {
+      type = 'server',
+      port = '${port}',
+      executable = {
+        command = vim.fn.stdpath 'data' .. '/mason/bin/codelldb',
+        args = { '--port', '${port}' },
+      },
+    }
+
+    dap.configurations.c = {
+      {
+        name = 'Launch Shell',
+        type = 'codelldb',
+        request = 'launch',
+        program = '${workspaceFolder}/main',
+        cwd = '${workspaceFolder}',
+        stopOnEntry = false,
+        terminal = 'integrated',
+      },
+    }
+    dap.configurations.cpp = dap.configurations.c
+  end,
+}
